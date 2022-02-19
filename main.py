05515e056d6ab501216f33a950a835e77c3c2caf
BEvgeniya <evgeniyaj88@gmail.com>
2022-02-19 17:32:37 +0500

    step 4 - checking for redirect

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 import requests
 import os
 
-url = "https://dvmn.org/filer/canonical/1542890876/16/"
+from requests import HTTPError
 
-response = requests.get(url)
-response.raise_for_status()
 
-filename = 'dvmn.svg'
-with open(filename, 'wb') as file:
-    file.write(response.content)
+def check_for_redirect(response):
+    if response.status_code != 200:
+        raise HTTPError("Achtung! Redirect detekted!!! Capitulir")
 
-for id in range(10):
-    book_id = id + 1
-    url_book = "http://tululu.org/txt.php?id=" + str(book_id)
 
-    response = requests.get(url_book)
-    response.raise_for_status()
-
+def save_book(book_id, response):
     filename = "id" + str(book_id) + ".txt"
-
     dirname = "books"
+
     if not os.path.exists(dirname):
         os.makedirs(dirname)
 
     with open(dirname + "\\" + filename, 'wb') as file:
         file.write(response.content)
+
+
+def main():
+    for id in range(10):
+        book_id = id + 1
+        url_book = "http://tululu.org/txt.php?id=" + str(book_id)
+
+        response = requests.get(url_book, allow_redirects=False)
+        try:
+            response.raise_for_status()
+            check_for_redirect(response)
+            save_book(book_id, response)
+        except HTTPError:
+            print("Unable to download file ", url_book)
+
+
+main()
