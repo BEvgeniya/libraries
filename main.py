6324a298e8002d77d094de371d7075400efa873f
BEvgeniya <evgeniyaj88@gmail.com>
2022-04-10 23:26:51 +0500

    step 8 - download books with titles

--- main.py
+++ main.py
@@ -5,38 +5,59 @@ from pathvalidate import sanitize_filename
 from requests import HTTPError
 
 
-def check_for_redirect(response):
-    if response.status_code != 200:
-        raise HTTPError("Achtung! Redirect detekted!!! Capitulir")
+def check_for_redirect(url, response):
+    if url != response.url:
+        raise HTTPError("Redirect detected. Download canceled")
 
 
-def download_txt(url, filename, folder='books'):
-    # """Функция для скачивания текстовых файлов.
-    # Args:
-    #     url (str): Cсылка на текст, который хочется скачать.
-    #     filename (str): Имя файла, с которым сохранять.
-    #     folder (str): Папка, куда сохранять.
-    # Returns:
-    #     str: Путь до файла, куда сохранён текст.
-    # """
-
+def http_request(url):
     response = requests.get(url, allow_redirects=True)
     try:
         response.raise_for_status()
-    except HTTPError:
-        print("Unable to download file ", url)
+        check_for_redirect(url, response)
+    except HTTPError as e:
+        print("Unable to download file ", url, ' cause ', str(e))
+        return None
 
-    # soup = BeautifulSoup(response.text, 'lxml')
-    # header = soup.find('h1')
-    # title, author = header.text.split('::')
+    return response
 
-    filename = sanitize_filename(filename) + '.txt'
-    folder = sanitize_filename(folder)
+def parse_title(url, book_id, folder='books'):
+    response = http_request(url)
+    if response == None:
+        return ''
 
-    filepath = os.path.join(folder, filename)
+    soup = BeautifulSoup(response.text, 'lxml')
+    header = soup.find('h1')
+
+    titles = header.text.split('::')
+    if len(titles) == 0:
+        title = header.text
+    else:
+        title = titles[0]
+
+    title = sanitize_filename(title.strip())
+    folder = sanitize_filename(folder.strip())
 
     if not os.path.exists(folder):
         os.makedirs(folder)
+
+    filename = str(book_id) + '. ' + title + '.txt'
+
+    return os.path.join(folder, filename)
+
+
+def download_txt(url, filepath):
+    # """Функция для скачивания текстовых файлов.
+    # Args:
+    #     url (str): Cсылка на текст, который хочется скачать.
+    #     filepath (str): Полный путь сохраняемого файла
+    # Returns:
+    #     str: Путь до файла, куда сохранён текст. Имя файла формируется из заголовка скачанной книги
+    # """
+
+    response = http_request(url)
+    if response == None:
+        return ''
 
     with open(filepath, 'wb') as file:
         file.write(response.content)
@@ -44,33 +65,17 @@ def download_txt(url, filename, folder='books'):
     return filepath
 
 
-# def main():
-    # for id in range(10):
-    #     book_id = id + 1
-    #     url_book = "http://tululu.org/txt.php?id=" + str(book_id)
-    #
-    #     response = requests.get(url_book, allow_redirects=False)
-    #     try:
-    #         response.raise_for_status()
-    #         check_for_redirect(response)
-    #         save_book(book_id, response)
-    #     except HTTPError:
-    #         print("Unable to download file ", url_book)
+def main():
+    for id in range(10):
+        book_id = id + 1
+        url_site = "https://tululu.org/b" + str(book_id) + '/'
+        filename = parse_title(url_site, book_id)
+
+        if filename != '':
+            url_book = "https://tululu.org/txt.php?id=" + str(book_id)
+            download_txt(url_book, filename)
 
 
-def test():
-    # Примеры использования
-    url = 'http://tululu.org/txt.php?id=1'
-
-    filepath = download_txt(url, 'Алиби')
-    print(filepath)  # Выведется books/Алиби.txt
-
-    filepath = download_txt(url, 'Али/би', folder='books/')
-    print(filepath)  # Выведется books/Алиби.txt
-
-    filepath = download_txt(url, 'Али\\би', folder='txt/')
-    print(filepath)  # Выведется txt/Алиби.txt
+main()
 
 
-# main()
-test()
